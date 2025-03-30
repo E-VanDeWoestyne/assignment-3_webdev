65b6f33bb219dbe4ffe7f6567d1457bdfbd84fc7
Yash1071 <167578276+Yash1071@users.noreply.github.com>
2025-03-30 13:58:55 -0600

    Delete Footer.js

--- Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className="bg-gray-800 p-4 text-white mt-8 text-center">
-        <p>&copy; {new Date().getFullYear()} Internet Movies Rental Company</p>
-        <p>Contact: info@imr.com</p>
-      </footer>
-    );
-  }
